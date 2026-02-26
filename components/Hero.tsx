75049eaa5b914839a9bd3aa4cfa4977cb9dbd5ea
advogadovitordalben-wq <advogadovitordalben@gmail.com>
2026-02-26 20:23:27 -0300

    Remove unused button and comments from Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-// Button import removido pois não é mais usado
 
 const Hero: React.FC = () => {
   return (
     <section className="relative min-h-[85vh] flex items-center pt-20 overflow-hidden bg-black">
       
-      {/* IMAGEM DE FUNDO COM EFEITOS */}
+      {/* IMAGEM DE FUNDO */}
       <div className="absolute inset-0 z-0">
         <img 
           src="https://i.imgur.com/sBDyHFA.png" 
           alt="Dr. Vítor Dalben" 
           className="w-full h-full object-cover object-right md:object-center opacity-90"
         />
-        
-        {/* GRADIENTES */}
         <div className="absolute inset-0 bg-gradient-to-r from-black via-black/80 to-transparent z-10"></div>
         <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent z-10"></div>
       </div>
@@ -33,11 +30,8 @@ const Hero: React.FC = () => {
             A Justiça tem condenado empresas a pagarem danos morais por ligações excessivas de cobrança ou ofertas. Entenda quando é possível ser indenizado(a).
           </p>
           
-          {/* Botão removido conforme solicitado */}
+          {/* O AVISO FOI REMOVIDO DAQUI */}
           
-          <p className="text-sm text-gray-400 italic pt-4">
-            *Análise inicial confidencial e sem custo pelo advogado.
-          </p>
         </div>
       </div>
     </section>
